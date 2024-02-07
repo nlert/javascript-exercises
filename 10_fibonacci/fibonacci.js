6eb06e5c57245173a5b5afc2326223ceb8eed79a
nlert <neillertola@gmail.com>
2024-02-07 13:38:22 -0500

    FIb

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    const arr = [0, 1];
+    for (let i = 2; i <= a; i++) {
+        let num = arr[i - 1] + arr[i - 2];
 
+        arr.push(num);
+    }
+
+    let lastNum = arr.pop();
+    return lastNum;
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(6));
+console.log(fibonacci(10));
+console.log(fibonacci(1));
 
 // Do not edit below this line
 module.exports = fibonacci;
