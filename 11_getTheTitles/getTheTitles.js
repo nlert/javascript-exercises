cc4e8089111fd49f754f90aa2efefe61d6f954d3
nlert <neillertola@gmail.com>
2024-02-07 13:43:13 -0500

    fcv

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((book) => book.title)
 };
+
+console.log(getTheTitles(
+    books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]));
 
 // Do not edit below this line
 module.exports = getTheTitles;
