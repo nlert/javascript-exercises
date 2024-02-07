a62ab92d75d400027abd9ac297d7c1962991bfc3
nlert <neillertola@gmail.com>
2024-02-07 13:27:49 -0500

    Beauty

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,53 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    let sum = arr.reduce((sum, item) => {
+        sum = sum + item;
+    }, 0);
+
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (a, b) {
+    return a * b;
 };
 
-const power = function() {
-	
+const power = function (a, power) {
+    return a ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+    const arr = [];
+
+    for (let i = 1; i <= a; i++) {
+        arr.push(i);
+    }
+
+    let factorial = arr.reduce((total, item) => {
+        return total * item;
+    }, 1);
+
+    return factorial;
 };
+
+console.log(`0: ${factorial(0)}`);
+console.log(`1: ${factorial(1)}`);
+console.log(`5: ${factorial(5)}`);
+
+console.log(`3 pow 5: ${power(3, 5)}`);
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stripString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                            .replace(/\s/g, "")
+                            .toUpperCase();
 
+    for (let i = 0; i < stripString.length; i++) {
+        if (stripString[i] !== stripString[stripString.length - 1 - i]) {
+            return false;
+        } else {
+            continue;
+        }
+    }
+
+    return true;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
